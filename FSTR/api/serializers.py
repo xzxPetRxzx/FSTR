feb4d3559bf1e635c06a9569626dad9d9511133b
xzxPetRxzx <fat-bee@mail.ru>
2022-12-11 18:10:36 +1000

    Добавлены все методы

--- FSTR/api/serializers.py
+++ FSTR/api/serializers.py
@@ -50,7 +50,7 @@ class PerevalAddedSerializer(serializers.Serializer):
         saved_level = Level.objects.create(**level)
         pereval = PerevalAdded.objects.create(**validated_data, user=saved_user[0], level=saved_level, coords=saved_coords)
         for image in images:
-            PerevalImages.objects.create(**image, pereval_added=pereval )
+            PerevalImages.objects.create(**image, pereval_added=pereval)
         return pereval
 
     def update(self, instance, validated_data):
@@ -64,6 +64,19 @@ class PerevalAddedSerializer(serializers.Serializer):
         coords_obj.longitude = coords.get("longitude", coords_obj.longitude)
         coords_obj.height = coords.get("height", coords_obj.height)
         coords_obj.save()
+        level_obj.winter = level.get("winter", level_obj.winter)
+        level_obj.summer = level.get("summer", level_obj.summer)
+        level_obj.autumn = level.get("autumn", level_obj.autumn)
+        level_obj.spring = level.get("spring", level_obj.spring)
+        level_obj.save()
+        for image in images:
+            if images_obj != []:
+                image_obj = images_obj.pop(0)
+                image_obj.title = image.get("title", image_obj.title)
+                image_obj.data = image.get("data", image_obj.data)
+                image_obj.save()
+            else:
+                PerevalImages.objects.create(**image, pereval_added=instance)
         instance.beauty_title = validated_data.get("beauty_title", instance.beauty_title)
         instance.title = validated_data.get("title", instance.title)
         instance.other_titles = validated_data.get("other_titles", instance.other_titles)
